7714f0b2d68c61aceab5bc135d968f2049ca63de
Tadeáš Valenta <tadashicz@gmail.com>
2018-04-03 16:08:59 +0200

    lab02-b done

--- lab02-b/main.cpp
+++ lab02-b/main.cpp
@@ -3,91 +3,84 @@
 #include <string.h>
 
 int main(void) {
-  int res;
-  unsigned char ot[1024] = "abcdefghijklmnopqrstuvwxyz0123";  // open text
-  unsigned char ot2[1024] = "Můj text pro rc5.";  // open text
-  unsigned char st[1024] = "06fb7405eba8d9e94fb1f28f0dd21fdec55fd54750ee84d95ecccf2b1b48";  // sifrovany text
-  unsigned char st2[1024] = "33f6630eaea4dba152baf38d019c04cbc759c94544fb9a815dc68d7b5f1a";  // sifrovany text
-  unsigned char dt[1024];
-  unsigned char key[EVP_MAX_KEY_LENGTH] = "Muj klic";  // klic pro sifrovani
-  unsigned char iv[EVP_MAX_IV_LENGTH] = "a";  // inicializacni vektor
-  unsigned char iv2[EVP_MAX_IV_LENGTH] = "adasdasdasdasa";  // inicializacni vektor
-  const char cipherName[] = "RC4";
-  const EVP_CIPHER * cipher;
-  int stLength = 60;
-/*
-  OpenSSL_add_all_ciphers();
-  /* sifry i hashe by se nahraly pomoci OpenSSL_add_all_algorithms()
-  cipher = EVP_get_cipherbyname(cipherName);
-  if(!cipher) {
-    printf("Sifra %s neexistuje.\n", cipherName);
-    exit(1);
-  }
+	int res;
+	unsigned char ot[1024] = "abcdefghijklmnopqrstuvwxyz0123";  // open text
+	unsigned char ot2[1024] = "Naprosto tajny text, který je fakt hrozně úžasný, takže si ho musíte přečíst";
+	unsigned char st[1024];  // sifrovany text
+	unsigned char st2[1024];
+	unsigned char newText[1024];
+	unsigned char key[EVP_MAX_KEY_LENGTH] = "Supertajnyklic";  // klic pro sifrovani
+	unsigned char iv[EVP_MAX_IV_LENGTH] = "inicial. vektor";  // inicializacni vektor
+	const char cipherName[] = "RC4";
+	const EVP_CIPHER * cipher;
 
-  int otLength = strlen((const char*) ot);
-  int stLength = 0;
-  int stLength2 = 0;
-  int tmpLength = 0;
+	OpenSSL_add_all_ciphers();
+	/* sifry i hashe by se nahraly pomoci OpenSSL_add_all_algorithms() */
+	cipher = EVP_get_cipherbyname(cipherName);
+	if(!cipher) {
+		printf("Sifra %s neexistuje.\n", cipherName);
+		exit(1);
+	}
 
-  EVP_CIPHER_CTX *ctx; // context structure
-  ctx = EVP_CIPHER_CTX_new();
-  if (ctx == NULL) exit(2);
+	int otLength = strlen((const char*) ot);
+	int stLength = 0;
+	int tmpLength = 0;
 
-  printf("OT: %s\n", ot);
+	EVP_CIPHER_CTX *ctx; // context structure
+	ctx = EVP_CIPHER_CTX_new();
+	if (ctx == NULL) exit(2);
 
-  /*
-  res = EVP_EncryptInit_ex(ctx, cipher, NULL, key, iv);  // context init - set cipher, key, init vector
-  if(res != 1) exit(3);
-  res = EVP_EncryptUpdate(ctx,  st, &tmpLength, ot, otLength);  // encryption of pt
-  if(res != 1) exit(4);
-  stLength += tmpLength;
-  res = EVP_EncryptFinal_ex(ctx, st + stLength, &tmpLength);  // get the remaining ct
-  if(res != 1) exit(5);
-  stLength += tmpLength;
+	printf("OT: %s\n", ot);
 
-  tmpLength = 0;
+	/* Sifrovani */
+	res = EVP_EncryptInit_ex(ctx, cipher, NULL, key, iv);  // context init - set cipher, key, init vector
+	if(res != 1) exit(3);
+	res = EVP_EncryptUpdate(ctx,  st, &tmpLength, ot, otLength);  // encryption of pt
+	if(res != 1) exit(4);
+	stLength += tmpLength;
+	res = EVP_EncryptFinal_ex(ctx, st + stLength, &tmpLength);  // get the remaining ct
+	if(res != 1) exit(5);
+	stLength += tmpLength;
 
-  res = EVP_EncryptInit_ex(ctx, cipher, NULL, key, iv2);  // context init - set cipher, key, init vector
-  if(res != 1) exit(3);
-  res = EVP_EncryptUpdate(ctx,  st2, &tmpLength, ot2, otLength);  // encryption of pt
-  if(res != 1) exit(4);
-  stLength2 += tmpLength;
-  res = EVP_EncryptFinal_ex(ctx, st2 + stLength, &tmpLength);  // get the remaining ct
-  if(res != 1) exit(5);
-  stLength2 += tmpLength;
+	printf ("Zasifrovano %d znaku.\n", stLength);
+	ctx = EVP_CIPHER_CTX_new();
+	otLength = strlen((const char*) ot2);
+	tmpLength = 0;
+	stLength = 0;
 
-  printf ("Zasifrovano %d znaku.\n", stLength);
+	res = EVP_EncryptInit_ex(ctx, cipher, NULL, key, iv);  // context init - set cipher, key, init vector
+	if(res != 1) exit(3);
+	res = EVP_EncryptUpdate(ctx,  st2, &tmpLength, ot2, otLength);  // encryption of pt
+	if(res != 1) exit(4);
+	stLength += tmpLength;
+	res = EVP_EncryptFinal_ex(ctx, st2 + stLength, &tmpLength);  // get the remaining ct
+	if(res != 1) exit(5);
+	stLength += tmpLength;
 
-  /* Desifrovani
-  res = EVP_DecryptInit_ex(ctx, cipher, NULL, key, iv);  // nastaveni kontextu pro desifrovani
-  if(res != 1) exit(6);
-  res = EVP_DecryptUpdate(ctx, dt, &tmpLength,  st, stLength);  // desifrovani st
-  if(res != 1) exit(7);
-  otLength += tmpLength;
-  res = EVP_DecryptFinal_ex(ctx, dt + otLength, &tmpLength);  // dokonceni (ziskani zbytku z kontextu)
-  if(res != 1) exit(8);
-  otLength += tmpLength;
+	printf ("Zasifrovano %d znaku.\n", stLength);
 
-  /* Desifrovani 2
-  res = EVP_DecryptInit_ex(ctx, cipher, NULL, key, iv);  // nastaveni kontextu pro desifrovani
-  if(res != 1) exit(6);
-  res = EVP_DecryptUpdate(ctx, ot, &tmpLength,  st, stLength);  // desifrovani st
-  if(res != 1) exit(7);
-  otLength += tmpLength;
-  res = EVP_DecryptFinal_ex(ctx, ot + otLength, &tmpLength);  // dokonceni (ziskani zbytku z kontextu)
-  if(res != 1) exit(8);
-  otLength += tmpLength;
+	/* Desifrovani */
+	res = EVP_DecryptInit_ex(ctx, cipher, NULL, key, iv);  // nastaveni kontextu pro desifrovani
+	if(res != 1) exit(6);
+	res = EVP_DecryptUpdate(ctx, ot, &tmpLength,  st, stLength);  // desifrovani st
+	if(res != 1) exit(7);
+	otLength += tmpLength;
+	res = EVP_DecryptFinal_ex(ctx, ot + otLength, &tmpLength);  // dokonceni (ziskani zbytku z kontextu)
+	if(res != 1) exit(8);
+	otLength += tmpLength;
+
+	/* Clean up */
+	EVP_CIPHER_CTX_free(ctx);
+	printf ("Zasifrovano %d znaku.\n", stLength);
 
 
-  /* Clean up
-  EVP_CIPHER_CTX_free(ctx);*/
-  char secondText[stLength];
-  /* Vypsani zasifrovaneho a rozsifrovaneho textu. */
-  for (int i = 0 ; i < stLength ; i++){
-    secondText[i]=st[i]^ot[i];
-  }
+	/* Vypsani zasifrovaneho a rozsifrovaneho textu. */
+	printf("ST1: %s\nDecryptT1: %s\n, ST2: %s\n", st, ot, st2);
 
-  printf("ST: %s\nST2: %s\nDT: %s\n", st, st2, dt);
-  printf("SecondText: %s\n", secondText);
-  exit(0);
+	for (int i = 0 ; i < 30 ; i++){
+		newText[i] = st2[i]^ot[i]^st[i];
+	}
+
+	printf("DecryptT2: %s\n", newText);
+	exit(0);
 }
